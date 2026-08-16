39fb8fb71be001e48425e23df8b8bcf9d0e3dcd2
DevAkshat21 <akshat.2108.07@gmail.com>
2026-08-16 14:50:35 +0530

    combined the terms with one bit difference

--- main.c
+++ main.c
@@ -58,7 +58,11 @@ int main(int argc, char *argv[]) {
     }
 
     int groups[num_of_variables +1][num_of_minterms][num_of_variables];
-    int grp_count[num_of_variables + 1] = {0};
+
+    int grp_count[num_of_variables + 1];
+    for (int i = 0; i <= num_of_variables; i++) {
+        grp_count[i] = 0;
+    }
 
     for (int i = 0; i < num_of_minterms; i++) {
 
@@ -70,7 +74,42 @@ int main(int argc, char *argv[]) {
     }
 
     grp_count[group]++;
-    
+
     }
 
+    for (int i = 0; i < num_of_variables; i++) {
+
+        for (int j = 0; j < grp_count[i]; j++) {
+
+            for (int k = 0; k < grp_count[i + 1]; k++) {
+
+                int temp_binary[num_of_variables];
+                int differences = 0;
+
+                for (int l = 0; l < num_of_variables; l++) {
+
+                    if (groups[i][j][l] == groups[i + 1][k][l]) {
+                        temp_binary[l] = groups[i][j][l];
+                    } else {
+                        temp_binary[l] = -1;   // -1 represents '-'
+                        differences++;
+                    }
+                }
+
+                if (differences == 1) {
+
+                    printf("Combined term: ");
+
+                    for (int l = 0; l < num_of_variables; l++) {
+                        if (temp_binary[l] == -1)
+                            printf("-");
+                        else
+                            printf("%d", temp_binary[l]);
+                    }
+
+                    printf("\n");
+                }
+            }
+        }
+    }
 }
