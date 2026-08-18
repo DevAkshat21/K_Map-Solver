d9f1023427970990e76e81d69787e1ac05620c84
DevAkshat21 <akshat.2108.07@gmail.com>
2026-08-18 23:59:18 +0530

    second groupin itteration

--- main.c
+++ main.c
@@ -77,6 +77,9 @@ int main(int argc, char *argv[]) {
 
     }
 
+    int combined[num_of_minterms][num_of_variables];
+    int combined_count = 0;
+
     for (int i = 0; i < num_of_variables; i++) {
 
         for (int j = 0; j < grp_count[i]; j++) {
@@ -91,23 +94,118 @@ int main(int argc, char *argv[]) {
                     if (groups[i][j][l] == groups[i + 1][k][l]) {
                         temp_binary[l] = groups[i][j][l];
                     } else {
-                        temp_binary[l] = -1;   // -1 represents '-'
+                        temp_binary[l] = -1;
                         differences++;
                     }
                 }
 
                 if (differences == 1) {
-
-                    printf("Combined term: ");
-
                     for (int l = 0; l < num_of_variables; l++) {
-                        if (temp_binary[l] == -1)
-                            printf("-");
-                        else
-                            printf("%d", temp_binary[l]);
+                        combined[combined_count][l] = temp_binary[l];
                     }
 
-                    printf("\n");
+                    combined_count++;
+                    }
+                }
+            }
+
+        }
+    
+    int combined_ones_count[combined_count];
+
+    for (int i = 0; i < combined_count; i++) {
+        combined_ones_count[i] = 0;
+
+        for (int j = 0; j < num_of_variables; j++) {
+            if (combined[i][j] == 1) {
+                combined_ones_count[i]++;
+            }
+        }
+    }
+
+    int combined_groups[num_of_variables + 1][combined_count][num_of_variables];
+
+    int combined_grp_count[num_of_variables + 1];
+
+    for (int i = 0; i <= num_of_variables; i++) {
+    combined_grp_count[i] = 0;
+    }
+
+
+    for (int i = 0; i < combined_count; i++) {
+
+        int group = combined_ones_count[i];
+        int position = combined_grp_count[group];
+
+        for (int j = 0; j < num_of_variables; j++) {
+            combined_groups[group][position][j] = combined[i][j];
+        }
+
+        combined_grp_count[group]++;
+    }
+
+    int combined_iteration_2[combined_count][num_of_variables];
+    int combined_iteration_2_count = 0;
+
+
+    for (int i = 0; i < num_of_variables; i++) {
+
+        for (int j = 0; j < combined_grp_count[i]; j++) {
+
+            for (int k = 0; k < combined_grp_count[i + 1]; k++) {
+
+                int temp_binary[num_of_variables];
+                int difference = 0;
+                int valid = 1;
+
+
+                for (int l = 0; l < num_of_variables; l++) {
+
+                    int bit1 = combined_groups[i][j][l];
+                    int bit2 = combined_groups[i + 1][k][l];
+
+
+                    /* Both are already don't-cares */
+
+                    if (bit1 == -1 && bit2 == -1) {
+                        temp_binary[l] = -1;
+                    }
+
+
+                    /* One is don't-care and the other isn't */
+
+                    else if (bit1 == -1 || bit2 == -1) {
+                        valid = 0;
+                        break;
+                    }
+
+
+                    /* Both are normal bits */
+
+                    else if (bit1 == bit2) {
+                        temp_binary[l] = bit1;
+                    }
+
+
+                    /* 0 vs 1 */
+
+                    else {
+                        temp_binary[l] = -1;
+                        difference++;
+                    }
+                }
+
+
+                /* one new difference */
+
+                if (valid && difference == 1) {
+
+                    for (int l = 0; l < num_of_variables; l++) {
+                        combined_iteration_2[combined_iteration_2_count][l]
+                            = temp_binary[l];
+                    }
+
+                    combined_iteration_2_count++;
                 }
             }
         }
